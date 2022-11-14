b41da2fd322269815e219d7c9972240fd7621f4c
Jo <Jo@DESKTOP-NI8KIGM>
2022-11-14 23:49:04 +0900

    2022.11.14 homework

--- chapter11/src/com/yedam/java/ch1101/StringExample2.java
+++ chapter11/src/com/yedam/java/ch1101/StringExample2.java
@@ -1,26 +1,28 @@
 package com.yedam.java.ch1101;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class StringExample2 {
 	public static void main(String[] args) {
 		
 		// 1) 문자열 뒤집기
-		// 1-1. StringBuffer와 reverse 사용
-		String str1 = "abc";
-		
-		StringBuffer re = new StringBuffer(str1);
-		String reverse = re.reverse().toString();
-		
-		System.out.println(re);
-		System.out.println(reverse);
-		
-		
-		// 1-2. StringBuffer 사용
-		String str2 = "ghr";
-		StringBuffer sb = new StringBuffer();
-		sb.append(str2);
-		
-		System.out.println(sb.reverse());
-		
+//		// 1-1. StringBuffer와 reverse 사용
+//		String str1 = "abc";
+//		
+//		StringBuffer re = new StringBuffer(str1);
+//		String reverse = re.reverse().toString();
+//		
+//		System.out.println(re);
+//		System.out.println(reverse);
+//		
+//		
+//		// 1-2. StringBuffer 사용
+//		String str2 = "ghr";
+//		StringBuffer sb = new StringBuffer();
+//		sb.append(str2);
+//		
+//		System.out.println(sb.reverse());
 		
 		
 		// 2. for문 사용, 
@@ -55,6 +57,8 @@ public class StringExample2 {
 		System.out.printf("숫자 %d개, 영어 %d개", countNum, countAp);
 		
 		
+		System.out.println();
+		
 		// 3) 생년월일 6자리를 입력하였을 경우 만 나이를 구하는 프로그램 작성
 //		조건> 금년 기준으로 100년이 넘어간 경우는 제외한다. 또한 생일은 신경쓰지 않는다.
 //		(221114 -> 1922년 11월 14일 X, 2022년 11월 14일O)
@@ -62,8 +66,20 @@ public class StringExample2 {
 		
 		String bd = "000510";
 		
-		int year = Integer.parseInt(bd.substring(0,3));
+		int year = Integer.parseInt(bd.substring(0,2));
 		
+		int currentYear = 2022;
+		
+		int manAge = 0;
+		
+		
+		if(year >= 23) { 
+			year += 1900;
+		}else{
+			year += 2000;
+		}
+		manAge = currentYear - year;
+		System.out.println("만나이: " + manAge + "살");
 		
 		
 		
